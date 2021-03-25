97c15a19976e26cc857a834d32f326af33ad6c8e
Gregory N. Schmit <schmitgreg@gmail.com>
2021-03-25 17:38:20 -0500

    Simplify even more.

--- Gemfile
+++ Gemfile
@@ -1,27 +1,15 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.5'
-# Use Puma as the app server
 gem 'puma', '~> 4.1'
+gem 'sqlite3'
+
 # Lock at 5.1 to prevent using `sassc`, which induces ridiculous build times.
 gem 'sass-rails', '5.1'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.7'
-# Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
-# Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+# lock to ~> 1.10.0 for platform compatibility
+gem 'nokogiri', '~> 1.10.0'
 
-gem 'nokogiri', '~> 1.10.0'  # lock to ~> 1.10.0 for platform compatibility
-
-# Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-gem 'web-console', '>= 3.3.0'
-gem 'listen', '~> 3.2'
 gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-
 gem 'active_scaffold', git: 'https://github.com/activescaffold/active_scaffold', ref: '810e1fe'
--- config/application.rb
+++ config/application.rb
@@ -21,14 +21,41 @@ Bundler.require(*Rails.groups)
 
 module GcloudBug
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     config.hosts = nil
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.action_dispatch.show_exceptions = false
+    config.consider_all_requests_local = true
+    config.action_controller.allow_forgery_protection = false
+
+    config.session_store :cookie_store, :key => '_session'
+    config.secret_token = 'a_test_token'
+    config.secret_key_base = 'a_test_secret'
+
+    config.cache_classes = false
+    config.eager_load = false
+
+    if Rails.root.join('tmp', 'caching-dev.txt').exist?
+      config.action_controller.perform_caching = true
+      config.action_controller.enable_fragment_cache_logging = true
+      config.cache_store = :memory_store
+      config.public_file_server.headers = {
+        'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      }
+    else
+      config.action_controller.perform_caching = false
+      config.cache_store = :null_store
+    end
+
+    config.active_support.deprecation = :log
+
+    config.assets.debug = true
+    config.assets.quiet = true
+    config.assets.check_precompiled_asset = false
+
+    config.serve_static_files = true
+
+    # config.logger = Logger.new($stdout)
+    # config.log_level = :warn
   end
 end
